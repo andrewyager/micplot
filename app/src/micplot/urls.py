4f8fef45ac0b6ea3fcd0a194cb078fa1aac56572
Andrew Yager <andrew@rwts.com.au>
2019-10-13 18:58:47 +1100

    Change Admin name

--- app/src/micplot/urls.py
+++ app/src/micplot/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
+admin.site.site_header = 'MicPlot'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^jet/', include('jet.urls', 'jet')),
